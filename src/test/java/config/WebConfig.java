c046e6037f5df987a4f97155f729f182b2f34070
TMaksyutov <dadojkeee@mail.ru>
2022-12-24 21:52:59 +0300

    added WebConfig

---
+++ src/test/java/config/WebConfig.java
@@ -0,0 +1,35 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "classpath:remote.properties"
+})
+public interface WebConfig extends Config {
+
+    @Key("baseUrl")
+    @DefaultValue("https://ufa.hh.ru")
+    String baseUrl();
+
+    @Key("browser")
+    @DefaultValue("chrome")
+    String browser();
+
+    @Key("browserSize")
+    @DefaultValue("1920x1080")
+    String browserSize();
+
+    @Key("browserVersion")
+    String browserVersion();
+
+    @Key("remote")
+    @DefaultValue("false")
+    String remote();
+
+    String selenoidLogin();
+
+    String selenoidPassword();
+
+}
+
+
--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,48 +1,28 @@
 package tests;
 
-import com.codeborne.selenide.Configuration;
-
-import config.TestsConfig;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import drivers.WebDriverProvider;
 import helpers.Attach;
-import org.aeonbits.owner.ConfigFactory;
+import io.qameta.allure.selenide.AllureSelenide;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
+    static String useRemote = System.getProperty("webPlatform", "remote");
+
 
     @BeforeAll
     static void setUp() {
-        TestsConfig config = ConfigFactory.create(TestsConfig.class, System.getProperties());
+        WebDriverProvider.configure();
+    }
 
-        String browserName = String.valueOf(config.browser());
-        String browserVersion = config.version();
-        String browserResolution = config.resolution();
-
-        Configuration.browser = browserName;
-        Configuration.browserVersion = browserVersion;
-        Configuration.baseUrl = config.baseUrl();
-        Configuration.browserSize = browserResolution;
-
-        if (config.remote()) {
-            String selenoidLogin = config.selenoidLogin(),
-                    selenoidPassword = config.selenoidPassword();
-
-            Configuration.remote = String.format("https://%s:%s@selenoid.autotests.cloud/wd/hub",
-                    selenoidLogin, selenoidPassword);
-
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setCapability("enableVNC", true);
-            capabilities.setCapability("enableVideo", true);
-            Configuration.browserCapabilities = capabilities;
-        }
-
-        Attach.attachAsText("Browser: ", browserName);
-        Attach.attachAsText("Version: ", browserVersion);
-        Attach.attachAsText("Remote: ", String.valueOf(config.remote()));
-        Attach.attachAsText("Login: ", config.selenoidLogin());
+    @BeforeEach
+    public void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterEach
