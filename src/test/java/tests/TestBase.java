91dc94caa7c119423fff01a7043c1f70aec09186
TMaksyutov <dadojkeee@mail.ru>
2022-12-25 18:13:36 +0300

    edit TestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -8,10 +8,14 @@ import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import tests.pages.LoginPage;
+import tests.pages.MainPage;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
 public class TestBase {
+    MainPage mainPage = new MainPage();
+    LoginPage loginPage = new LoginPage();
 
     @BeforeAll
     static void setUp() {
