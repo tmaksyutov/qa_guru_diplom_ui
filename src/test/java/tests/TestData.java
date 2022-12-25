39fc681d292312c45452405aaf256e65e959311d
TMaksyutov <dadojkeee@mail.ru>
2022-12-25 18:13:23 +0300

    added TestData

---
+++ src/test/java/tests/TestData.java
@@ -0,0 +1,25 @@
+package tests;
+
+import drivers.WebDriver;
+import drivers.DriverUtils;
+
+import static com.codeborne.selenide.Selenide.title;
+
+public class TestData {
+
+    public static final String
+            baseUrl = "https://ufa.hh.ru",
+            email = WebDriver.config.hhruEmail(),
+            password = WebDriver.config.hhruPassword(),
+            invalidEmail = "timur@mail.ru",
+            invalidPassword = "123456",
+            loginUrl = "https://ufa.hh.ru/account/login?backurl=%2F%3FcustomDomain%3D1&hhtmFrom=main",
+            country = "Россия",
+            vacancy = "QA",
+            expectedTitle = "Работа в Уфе, поиск персонала и публикация вакансий - ufa.hh.ru",
+            actualTitle = title(),
+            consoleLogs = DriverUtils.getConsoleLogs(),
+            errorText = "SEVERE",
+            loginMassage = "Неправильные данные для входа. Пожалуйста, попробуйте снова.";
+
+}
