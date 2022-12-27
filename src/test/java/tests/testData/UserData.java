c17a450b83fcd33dc0c8fff12475171c4609f960
TMaksyutov <dadojkeee@mail.ru>
2022-12-27 19:58:45 +0300

    added data faker

---
+++ src/test/java/tests/testData/UserData.java
@@ -0,0 +1,14 @@
+package tests.testData;
+
+import com.github.javafaker.Faker;
+
+import java.util.Locale;
+
+public class UserData {
+    private static final Faker faker = new Faker(new Locale("en"));
+
+    public static String invalidEmail = faker.internet().emailAddress();
+
+    public static String invalidPassword = faker.internet().password();
+
+}
